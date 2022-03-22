6dbe9ad6f2689777504dcffcb8c4baf59cae5c07
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-03-22 15:14:31 -0400

    fix: store argon2-hashed password instead of original

--- lib/csv.ml
+++ lib/csv.ml
@@ -1,22 +1,26 @@
 module LoadCsv (M : Models.Model_intf.Model) = struct
-  let load ?transform file_name (module Db : Caqti_lwt.CONNECTION) =
+  let load ?(transform = fun x -> x) file_name
+      (module Db : Caqti_lwt.CONNECTION) =
     let module CsvUtil = Csvfields.Csv.Record (M) in
     let data = CsvUtil.csv_load file_name in
     Lwt_list.iter_s
       (fun raw_entry ->
-        let entry =
-          match transform with Some f -> f raw_entry | None -> raw_entry
-        in
+        let entry = transform raw_entry in
         M.add entry (module Db))
       data
 end
 
 module UserCsv = LoadCsv (Models.User)
 
-let load_funcs = [ UserCsv.load ?transform:None "data/Users.csv" ]
+let load_funcs =
+  [
+    UserCsv.load
+      ~transform:(fun u ->
+        { email = u.email; password = Hasher.hash u.password })
+      "data/Users.csv";
+  ]
 
 let run_load (module Db : Caqti_lwt.CONNECTION) =
-  let load_funcs = [ UserCsv.load "data/Users.csv" ] in
   Lwt_list.iter_s
     (fun (load_func : (module Caqti_lwt.CONNECTION) -> unit Lwt.t) ->
       load_func (module Db))
---
+++ lib/hasher.ml
@@ -0,0 +1,37 @@
+module Hash = Argon2.ID
+
+let hash password = 
+  let hash_len = 16 in
+  let salt_len = 16 in
+  let t_cost = 2 in
+  let m_cost = 50 * 1024 in
+  let parallelism = 4 in
+  let salt = Dream.random(salt_len) in
+  let encoded_len =
+    Argon2.encoded_len
+      ~t_cost
+      ~m_cost
+      ~parallelism
+      ~salt_len
+      ~hash_len
+      ~kind:ID
+    in
+
+  let result =
+    Hash.hash_encoded
+      ~t_cost
+      ~m_cost
+      ~parallelism
+      ~pwd:password
+      ~salt
+      ~hash_len
+      ~encoded_len
+  in match result with
+  | Result.Ok encoded -> Hash.encoded_to_string encoded
+  | Result.Error e -> failwith (Printf.sprintf "Error Hashing: %s" (Argon2.ErrorCodes.message e))
+
+let verify hash password =
+  let result = Argon2.verify ~encoded:hash ~pwd:password ~kind:ID in
+  match result with
+  | Result.Ok matches -> matches
+  | Result.Error e -> failwith (Printf.sprintf "Error Verifying: %s" (Argon2.ErrorCodes.message e))
