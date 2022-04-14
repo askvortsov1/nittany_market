828724615d2ac8682b864194ad1eb689f989becb
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-04-14 16:57:30 -0400

    feat: add "all" function to ModelRepository

--- lib/models/model_intf.ml
+++ lib/models/model_intf.ml
@@ -19,6 +19,7 @@ module type ModelRepository = sig
 
   val add : t -> (module Caqti_lwt.CONNECTION) -> unit Lwt.t
   val get : key -> (module Caqti_lwt.CONNECTION) -> t option Lwt.t
+  val all : unit -> (module Caqti_lwt.CONNECTION) -> t list Lwt.t
 end
 
 module Make_ModelRepository (M : Model) = struct
@@ -32,7 +33,8 @@ module Make_ModelRepository (M : Model) = struct
   let add x (module Db : Caqti_lwt.CONNECTION) =
     let query =
       Caqti_request.exec M.caqti_types
-        (Printf.sprintf "INSERT INTO %s VALUES (%s)" M.table_name prepared_pattern)
+        (Printf.sprintf "INSERT INTO %s VALUES (%s)" M.table_name
+           prepared_pattern)
     in
     let%lwt unit_or_error = Db.exec query (M.caqtup_of_t x) in
     Caqti_lwt.or_fail unit_or_error
@@ -46,4 +48,14 @@ module Make_ModelRepository (M : Model) = struct
       let%lwt unit_or_error = Db.find_opt query email in
       let raw = Caqti_lwt.or_fail unit_or_error in
       Lwt.map (Option.map M.t_of_caqtup) raw
+
+  let all () =
+    let query =
+      Caqti_request.collect Caqti_type.unit M.caqti_types
+        (Printf.sprintf "SELECT * FROM %s" M.table_name)
+    in
+    fun (module Db : Caqti_lwt.CONNECTION) ->
+      let%lwt unit_or_error = Db.collect_list query () in
+      let raw = Caqti_lwt.or_fail unit_or_error in
+      Lwt.map (List.map M.t_of_caqtup) raw
 end
