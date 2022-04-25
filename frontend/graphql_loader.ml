e3dd3d0dd7170e38e293d9a00ac79669caac2787
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-04-25 06:26:41 -0400

    feat: support trigger to recompute query in Graphql Query Loader
    This is needed to "refresh" the page on URL changes

--- frontend/graphql_loader.ml
+++ frontend/graphql_loader.ml
@@ -4,18 +4,30 @@ open Bonsai.Let_syntax
 module G = Nittany_market_frontend_graphql
 
 let private_component :
-    type a view.
+    type a b view.
     (module Bonsai.Model with type t = a) ->
     a Effect.t Value.t ->
     view_while_loading:view Value.t ->
     (a Value.t -> view Computation.t) ->
+    (module Bonsai.Model with type t = b) ->
+    b Value.t ->
     view Computation.t =
- fun (module Model) effect ~view_while_loading computation ->
+ fun (module Model) effect ~view_while_loading computation
+     (module ReloadTrigger) reload_trigger ->
   let%sub response, set_response = Bonsai.state_opt [%here] (module Model) in
   let%sub on_activate =
     let%arr effect = effect and set_response = set_response in
     let%bind.Effect response_from_server = effect in
     set_response (Some response_from_server)
+  in
+  let on_change =
+    Value.map2 on_activate set_response ~f:(fun e set_resp _ ->
+        Effect.ignore_m @@ Effect.all [ set_resp None; e ])
+  in
+  let%sub () =
+    Bonsai.Edge.on_change [%here]
+      (module ReloadTrigger)
+      reload_trigger ~callback:on_change
   in
   let%sub () = Bonsai.Edge.lifecycle ~on_activate () in
   match%sub response with
@@ -36,20 +48,33 @@ module ForQuery (Q : G.Queries.Query) = struct
 
   let error_handler component =
     Client.(
-      function response -> match%sub response with
-      | Success body -> component body
-      | Unauthorized -> Bonsai.const @@ Vdom.Node.text "Error: Unauthorized"
-      | Forbidden -> Bonsai.const @@ Vdom.Node.text "Error: Forbidden"
-      | NotFound -> Bonsai.const @@ Vdom.Node.text "Error: Not Found"
-      | TooManyRequests ->
-          Bonsai.const
-          @@ Vdom.Node.text "Error: Too Many Requests, try again later."
-      | OtherError err -> let%arr err = err in Vdom.Node.text err)
+      function
+      | response -> (
+          match%sub response with
+          | Success body -> component body
+          | Unauthorized -> Bonsai.const @@ Vdom.Node.text "Error: Unauthorized"
+          | Forbidden -> Bonsai.const @@ Vdom.Node.text "Error: Forbidden"
+          | NotFound -> Bonsai.const @@ Vdom.Node.text "Error: Not Found"
+          | TooManyRequests ->
+              Bonsai.const
+              @@ Vdom.Node.text "Error: Too Many Requests, try again later."
+          | OtherError err ->
+              let%arr err = err in
+              Vdom.Node.text err))
 
-  let component inner qvars =
+  let component ?(trigger = Value.return "") inner qvars =
     let inner_handled = error_handler inner in
-    private_component
-      (module ClientResponse)
-      (Value.return (Effect_lwt.of_deferred_fun Client.query qvars))
-      ~view_while_loading inner_handled
+    let%sub inner =
+      private_component
+        (module ClientResponse)
+        (Value.map
+           ~f:(fun qvars -> Effect_lwt.of_deferred_fun Client.query qvars)
+           qvars)
+        ~view_while_loading inner_handled
+        (module String)
+        trigger
+    in
+    let%arr inner = inner and trigger = trigger in
+    ignore trigger;
+    inner
 end
--- frontend/graphql_loader.mli
+++ frontend/graphql_loader.mli
@@ -4,7 +4,8 @@ module G = Nittany_market_frontend_graphql
 
 module ForQuery (Q : G.Queries.Query) : sig
   val component :
+    ?trigger:string Value.t ->
     (Q.t Value.t -> Vdom.Node.t Computation.t) ->
-    Q.t_variables ->
+    Q.t_variables Value.t ->
     Vdom.Node.t Computation.t
 end
