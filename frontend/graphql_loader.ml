8f9ee9e4125dfcc5b171477fee8c002a108fd583
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-05-17 19:36:10 -0400

    feat: annotate graphql_loader, clear up code a bit

--- frontend/graphql_loader.ml
+++ frontend/graphql_loader.ml
@@ -4,36 +4,44 @@ open Bonsai.Let_syntax
 module G = Nittany_market_frontend_graphql
 
 let private_component :
-    type a b view.
-    (module Bonsai.Model with type t = a) ->
-    a Effect.t Value.t ->
+    type data view trigger.
     view_while_loading:view Value.t ->
-    (a Value.t -> view Computation.t) ->
-    (module Bonsai.Model with type t = b) ->
-    b Value.t ->
+    computation:(data Value.t -> view Computation.t) ->
+    (module Bonsai.Model with type t = data) ->
+    data Effect.t Value.t ->
+    (module Bonsai.Model with type t = trigger) ->
+    trigger Value.t ->
     view Computation.t =
- fun (module Model) effect ~view_while_loading computation
+ fun ~view_while_loading ~computation (module Model) effect
      (module ReloadTrigger) reload_trigger ->
+  (* The API response state is optional, since it won't be set until loaded. *)
   let%sub response, set_response = Bonsai.state_opt [%here] (module Model) in
+
   let%sub on_activate =
     let%arr effect = effect and set_response = set_response in
     let%bind.Effect response_from_server = effect in
     set_response (Some response_from_server)
   in
+  let%sub () = Bonsai.Edge.lifecycle ~on_activate () in
+
   let on_change =
-    Value.map2 on_activate set_response ~f:(fun e set_resp _ ->
-        Effect.ignore_m @@ Effect.all [ set_resp None; e ])
+    Value.map2 on_activate set_response ~f:(fun refetch set_resp _ ->
+        Effect.ignore_m (Effect.all [ set_resp None; refetch ]))
   in
   let%sub () =
     Bonsai.Edge.on_change [%here]
       (module ReloadTrigger)
       reload_trigger ~callback:on_change
   in
-  let%sub () = Bonsai.Edge.lifecycle ~on_activate () in
+
+  (* If the response has been loaded, we display the component.
+   * Otherwise, we show a loading indicator.
+   *)
   match%sub response with
   | None -> return view_while_loading
   | Some response -> computation response
 
+(* This is the concrete implementation of our query loader. *)
 module ForQuery (Q : G.Queries.Query) = struct
   module QSexpable = G.Queries.SerializableQuery (Q)
   module Client = G.Client.ForQuery (Q)
@@ -46,35 +54,36 @@ module ForQuery (Q : G.Queries.Query) = struct
 
   let view_while_loading = Value.return @@ Vdom.Node.text "Loading..."
 
-  let error_handler component =
+  let error_handler component response =
     Client.(
-      function
-      | response -> (
-          match%sub response with
-          | Success body -> component body
-          | Unauthorized -> Bonsai.const @@ Vdom.Node.text "Error: Unauthorized"
-          | Forbidden -> Bonsai.const @@ Vdom.Node.text "Error: Forbidden"
-          | NotFound -> Bonsai.const @@ Vdom.Node.text "Error: Not Found"
-          | TooManyRequests ->
-              Bonsai.const
-              @@ Vdom.Node.text "Error: Too Many Requests, try again later."
-          | OtherError err ->
-              let%arr err = err in
-              Vdom.Node.text err))
+      match%sub response with
+      | Success body -> component body
+      | Unauthorized -> Bonsai.const @@ Vdom.Node.text "Error: Unauthorized"
+      | Forbidden -> Bonsai.const @@ Vdom.Node.text "Error: Forbidden"
+      | NotFound -> Bonsai.const @@ Vdom.Node.text "Error: Not Found"
+      | TooManyRequests ->
+          Bonsai.const
+          @@ Vdom.Node.text "Error: Too Many Requests, try again later."
+      | OtherError err ->
+          let%arr err = err in
+          Vdom.Node.text err)
 
   let component ?(trigger = Value.return "") inner qvars =
+    (* Wraps the component in an error handler, since the
+     * type returned by the API client will be `ClientResponse`, not `Q.t` directly.
+     *)
     let inner_handled = error_handler inner in
-    let%sub inner =
-      private_component
-        (module ClientResponse)
-        (Value.map
-           ~f:(fun qvars -> Effect_lwt.of_deferred_fun Client.query qvars)
-           qvars)
-        ~view_while_loading inner_handled
-        (module String)
-        trigger
+    (* Map our query variables into an effect that fetches the query.
+     * `Effect_lwt.of_deffered_fun` turns an async function into an effect function.
+     *)
+    let fetch_query =
+      Value.map
+        ~f:(fun qvars -> Effect_lwt.of_deferred_fun Client.query qvars)
+        qvars
     in
-    let%arr inner = inner and trigger = trigger in
-    ignore trigger;
-    inner
+    private_component ~view_while_loading ~computation:inner_handled
+      (module ClientResponse)
+      fetch_query
+      (module String)
+      trigger
 end
